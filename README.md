15657991acbadcf5348bcffd1c6c802c5437b11f
Tariq P <tariqp1214@gmail.com>
2024-01-31 20:50:20 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Exercise3
 Exercise 3, 4639
+To be or not to be
---
+++ index.js
@@ -0,0 +1,13 @@
+
+let x = Math.floor(Math.random() * 3) + 1;
+
+
+if (x == 1) {
+   console.log("It always seems impossible until it's done")
+}
+if (x == 2) {
+    console.log("If you dream it, you can do it")
+} 
+if (x== 3) {
+    console.log("A rising tide lifts all ships")
+} 
