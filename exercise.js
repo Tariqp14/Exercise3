551507812d7c42b83305e36beefa131ac84528fc
Tariq P <tariqp1214@gmail.com>
2024-02-02 11:14:04 -0500

    Created quote app

---
+++ exercise.js
@@ -0,0 +1,19 @@
+
+//3-1
+
+let arrays = [[1, 2, 3], [4, 5], [6]];//blackbox AI
+    const newArray = arrays.reduce((accumulator, currentArray) => {
+
+    return accumulator.concat(currentArray);
+  
+  },[]);
+
+  console.log(newArray)
+
+
+//3-2
+
+//3-3
+
+//3-4
+
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 
-let x = Math.floor(Math.random() * 3) + 1;
-
-
-if (x == 1) {
-   console.log("It always seems impossible until it's done")
-}
-if (x == 2) {
-    console.log("If you dream it, you can do it")
-} 
-if (x== 3) {
-    console.log("A rising tide lifts all ships")
-} 
+let quotes = [
+    "It always seems impossible until it's done",
+    "If you dream it, you can do it",
+    "A rising tide lifts all ships"
+]
+console.log(quotes[Math.floor(Math.random()*3)])
